5b38be5aa5129d3dfc8b20399e7873640132c544
BrightonDube <BrightonDube@users.noreply.github.com>
2021-01-21 19:43:19 +0200

    add win check logic

--- src/Screens/GameScreen.js
+++ src/Screens/GameScreen.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import { StyleSheet, Text, View, Pressable, Alert } from "react-native";
+
 import NumberContainer from "../Components/NumberContainer";
 import ButtonContainer from "../Components/ButtonContainer";
 import Card from "../Components/Card";
@@ -14,54 +15,81 @@ const getRandomNumber = (min, max, exclude) => {
     return getRandomNumber(min, max, exclude);
   } else return ranNum;
 };
-const GameScreen = ({ myNum, screen }) => {
-  let minimum = 1;
-  let maximum = 100;
-  const [computerGuess, setComputerGuess] = useState(
-    getRandomNumber(minimum, maximum, myNum)
-  );
-  const [win, setWin] = useState(true);
 
-  const resultHandler = () => {
+const GameScreen = ({ myNum, screen }) => {
+  const minimum = useRef(1);
+  const maximum = useRef(100);
+  const [win, setWin] = useState(false);
+  const [rounds, setRounds] = useState(0);
+  const [computerGuess, setComputerGuess] = useState(
+    getRandomNumber(minimum.current, maximum.current, myNum)
+  );
+  useEffect(() => {
     if (computerGuess === myNum) {
       setWin(true);
+      return;
     }
-  };
+  }, [computerGuess]);
+
   const CompGuessHandler = (min, max, exclude) => {
     setComputerGuess(getRandomNumber(min, max, exclude));
-    resultHandler();
   };
+  const handleNextGuess = (direction) => {
+    if (
+      (direction === "lower" && computerGuess < myNum) ||
+      (direction === "higher" && computerGuess > myNum)
+    ) {
+      Alert.alert(
+        "Wrong hint",
+        "Please give a correct hint, this one is wrong!",
+        [{ text: "Sorry!", style: "cancel" }]
+      );
+      return;
+    }
+    if (direction === "lower") {
+      maximum.current = computerGuess;
+      CompGuessHandler(minimum.current, maximum.current, computerGuess);
+    } else {
+      minimum.current = computerGuess;
+      CompGuessHandler(minimum.current, maximum.current, computerGuess);
+    }
+    setRounds((currounds) => currounds + 1);
+  };
+
   return (
     <View>
       <Card style={styles.card}>
-        <Text style={{ fontSize: 24, alignSelf: "center" }}>
-          The computer's guess:{" "}
-        </Text>
-        <NumberContainer>{computerGuess}</NumberContainer>
-        <ButtonContainer>
-          <Pressable
-            style={styles.button}
-            onPress={() => CompGuessHandler(minimum, maximum, myNum)}
-          >
-            <Text style={styles.buttonText}>Lower</Text>
-          </Pressable>
-          <Pressable
-            style={styles.button}
-            onPress={() => CompGuessHandler(minimum, maximum, myNum)}
-          >
-            <Text style={styles.buttonText}>Higher</Text>
-          </Pressable>
-        </ButtonContainer>
         {win ? (
           <View>
             <Text style={{ alignSelf: "center" }}>
-              Game over, computer guessed your number {myNum}
+              Game over, computer guessed your number in {rounds} attempts!
             </Text>
             <Pressable style={styles.button} onPress={() => screen(false)}>
               <Text style={styles.buttonText}>Play Again</Text>
             </Pressable>
           </View>
-        ) : null}
+        ) : (
+          <View>
+            <Text style={{ fontSize: 24, alignSelf: "center" }}>
+              The computer's guess:{" "}
+            </Text>
+            <NumberContainer>{computerGuess}</NumberContainer>
+            <ButtonContainer>
+              <Pressable
+                style={styles.button}
+                onPress={() => handleNextGuess("lower")}
+              >
+                <Text style={styles.buttonText}>Lower</Text>
+              </Pressable>
+              <Pressable
+                style={styles.button}
+                onPress={() => handleNextGuess("higher")}
+              >
+                <Text style={styles.buttonText}>Higher</Text>
+              </Pressable>
+            </ButtonContainer>
+          </View>
+        )}
       </Card>
     </View>
   );
